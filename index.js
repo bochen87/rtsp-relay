2c56fa3240a2b2a64d0552797f2f45616faf81a2
Bo Chen <bo@enway.ai>
2022-09-14 09:54:51 +0200

    Update index.js

--- index.js
+++ index.js
@@ -47,9 +47,7 @@ class InboundStreamWrapper {
         ...(transport ? ['-rtsp_transport', transport] : []), // this must come before `-i [url]`, see #82
         '-i',
         url,
-        '-f', // force format
         'mpegts',
-        '-codec:v', // specify video codec (MPEG1 required for jsmpeg)
         'mpeg1video',
         '-r',
         '30', // 30 fps. any lower and the client can't decode it
